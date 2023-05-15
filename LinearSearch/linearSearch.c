2c1cf9c682b34ae341f9bb6e82138c717828c9e2
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-05-15 15:08:29 +0530

    Create linearSearch.c

---
+++ LinearSearch/linearSearch.c
@@ -0,0 +1,36 @@
+// C program to perform linear search on a dynamic array of integers
+#include <stdio.h>
+#include <stdlib.h>
+
+int linearSearch(int *array, int n, int element)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] == element)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, element;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    int *array = (int *)malloc(sizeof(int) * n);
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", array + i);
+
+    printf("Enter element to search: ");
+    scanf("%d", &element);
+
+    int pos = linearSearch(array, n, element);
+    if (pos < 0)
+        printf("Element not present in the array\n");
+    else
+        printf("Element found at position %d\n", pos);
+
+    return 0;
+}
