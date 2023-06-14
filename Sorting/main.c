a01e1fa75fa3de23a54da69ec27becb1c4b0cd06
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-06-14 11:52:31 +0530

    Update main.c

--- Sorting/main.c
+++ Sorting/main.c
@@ -1,5 +1,7 @@
 #include "bubbleSort.h"
 #include "insertionSort.h"
+#include "mergeSort.h"
+#include "quickSort.h"
 #include "selectionSort.h"
 #include "test.h"
 #include <stdio.h>
@@ -10,19 +12,23 @@ int main()
     // Create an array of sorting functions
     void (*sortingFunctions[])(int *, int) = {bubbleSort,
                                               selectionSort,
-                                              insertionSort};
+                                              insertionSort,
+                                              mergeSort,
+                                              quickSort};
 
     // Provide the names for the given sorting functions
     const char *sortingFunctionNames[] = {"Bubble Sort   ",
                                           "Selection Sort",
-                                          "Insertion Sort"};
+                                          "Insertion Sort",
+                                          "Merge Sort    ",
+                                          "Quick Sort    "};
 
     // Find the number of sorting functions present
     int numFunctions = sizeof(sortingFunctions) / sizeof(sortingFunctions[0]);
 
     // Test the sorting functions
     // test(sortingFunctions, sortingFunctionNames, numFunctions, nIter, min, max, count)
-    test(sortingFunctions, sortingFunctionNames, numFunctions, 10, 0, 10000, 1000);
+    test(sortingFunctions, sortingFunctionNames, numFunctions, 10, 0, 50000, 20000);
 
     return 0;
 }
